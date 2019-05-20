27cede98ac3a33798615a40fb697bc458bc0e4b3
CIDENET\bvillamizar <bvillamizar@cidenet.com.co>
2019-05-19 22:20:11 -0500

    proyecto1

---
+++ index.js
@@ -0,0 +1,8 @@
+const { obtenerpromedio, argv } = require('./datos')
+
+console.log(argv)
+if (argv._[0] == 'promedio') {
+    console.log('El promedio de ' + argv.n + ' es ')
+} else {
+    console.log('Promedio no calculado')
+}
